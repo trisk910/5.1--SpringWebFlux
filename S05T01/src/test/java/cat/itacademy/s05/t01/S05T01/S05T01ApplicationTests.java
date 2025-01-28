094fe72f5d26338cf5c8cff92df9442810140162
Marc Bernabeu Rodriguez <marc_b_r@hotmail.com>
2025-01-28 13:42:46 +0100

    creation of the project

---
+++ S05T01/src/test/java/cat/itacademy/s05/t01/S05T01/S05T01ApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.t01.S05T01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
