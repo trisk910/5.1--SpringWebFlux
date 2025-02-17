0ca0a3b23a9a9ed2e2985ba98c877745674b71d5
Marc Bernabeu Rodriguez <marc_b_r@hotmail.com>
2025-02-17 19:18:48 +0100

    Update GameService.java

--- BlackjackV2/src/main/java/cat/itacademy/s05/t01/n01/service/Game/GameService.java
+++ BlackjackV2/src/main/java/cat/itacademy/s05/t01/n01/service/Game/GameService.java
@@ -1,14 +1,4 @@
 package cat.itacademy.s05.t01.n01.service.Game;
-
-/*
-public interface GameService {
-    Mono<Game> createGame(String name);
-    Mono<Game> getGame(String id);
-    Mono<Void> deleteGame(String gameId);
-    Mono<Game> playGame(String gameId, Action action, int betAmount);
-    Mono<Game> updatePlayerName(String gameId, Player player);
-}
-*/
 import cat.itacademy.s05.t01.n01.Models.Game;
 import reactor.core.publisher.Mono;
 
