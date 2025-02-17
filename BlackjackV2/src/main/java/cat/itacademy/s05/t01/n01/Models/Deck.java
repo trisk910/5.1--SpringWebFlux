ccb2ff8818a57a6f10e6c883334c1eeb3d538cc3
Marc Bernabeu Rodriguez <marc_b_r@hotmail.com>
2025-02-17 19:18:01 +0100

    Update Deck.java

--- BlackjackV2/src/main/java/cat/itacademy/s05/t01/n01/Models/Deck.java
+++ BlackjackV2/src/main/java/cat/itacademy/s05/t01/n01/Models/Deck.java
@@ -1,34 +1,4 @@
 package cat.itacademy.s05.t01.n01.Models;
-/*import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-public class Deck {
-    private List<Card> cards;
-
-    public Deck() {
-        cards = new ArrayList<>();
-        for (Suit suit : Suit.values()) {
-            for (Rank rank : Rank.values()) {
-                cards.add(new Card(rank, suit));
-            }
-        }
-        shuffle();
-    }
-
-    public void shuffle() {
-        Collections.shuffle(cards);
-    }
-
-    public Card drawCard() {
-        return cards.remove(cards.size() - 1);
-    }
-
-    public List<Card> getCards() {
-        return cards;
-    }
-}*/
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
